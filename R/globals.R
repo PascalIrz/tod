5635ccc6ef25efdc1e866582e0586d4900a6dbee
PascalIrz <pascal.irz@gmail.com>
2021-04-07 15:52:53 +0200

    ajout lecture shp à la fonction osm_depts_osm()

--- R/globals.R
+++ R/globals.R
@@ -1,1 +1,2 @@
-utils::globalVariables(c('.', 'code_site', 'code_station', 'date_obs', 'grandeur_hydro', 'resultat_obs', 'sta_id'))
+utils::globalVariables(c('.', 'code_site', 'code_station', 'date_obs', 'grandeur_hydro', 'resultat_obs', 'sta_id',
+                         'code_insee'))
---
+++ R/osm_depts_tod.R
@@ -0,0 +1,42 @@
+#' Télécharger les contours des départements
+#'
+#' Cette fonction télécharge depuis la plateforme data.gouv.fr une couche simplifiée
+#'     des contours des départements et la décompresse. La source est OpenStreetMap.
+#'
+#' @param url URL de téléchargement.
+#' @param repertoire Chemin vers le répertoire où le fichier sera stocké.
+#'
+#' @importFrom utils download.file unzip
+#' @importFrom sf st_as_sf
+#' @importFrom rgdal readOGR
+#'
+#' @return Un objet sf polygone des départements.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' url <- "https://www.data.gouv.fr/fr/datasets/r/3096e551-c68d-40ce-8972-a228c94c0ad1"
+#' depts <- osm_depts_tod(url = url, repertoire = "raw_data")
+#' }
+osm_depts_tod <- function(url, repertoire) {
+
+  # Téchargement du shapefile des contours des départements
+  fichier_destination <- paste0(repertoire, "/departements.zip")
+
+  download.file(url = url,
+                destfile = fichier_destination,
+                mode = "wb")
+
+  # Décompression
+  unzip(zipfile = fichier_destination,
+        exdir = repertoire)
+
+  # Lecture avec rgdal::readOGR() qui gère mieux l'encodage que sf::read_sf()
+  shp_depts <- paste0(repertoire, "/departements-20140306-100m.shp")
+
+  depts <- rgdal::readOGR(shp_depts, use_iconv = TRUE) %>%
+    sf::st_as_sf()
+
+  depts
+
+}
