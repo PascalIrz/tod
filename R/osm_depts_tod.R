73279ab78613c8a34ab8cb5b2fa9175d3dc03f6a
PascalIrz <pascal.irz@gmail.com>
2021-04-15 08:09:23 +0200

    modif osm_depts_tod() pour permettre de choisir le CRS de sortie

--- R/osm_depts_tod.R
+++ R/osm_depts_tod.R
@@ -3,11 +3,13 @@
 #' Cette fonction télécharge depuis la plateforme data.gouv.fr une couche simplifiée
 #'     des contours des départements et la décompresse. La source est OpenStreetMap.
 #'
-#' @param url URL de téléchargement.
-#' @param repertoire Chemin vers le répertoire où le fichier sera stocké.
+#' @param url Texte. URL de téléchargement.
+#' @param repertoire Texte. Chemin vers le répertoire où le fichier sera stocké.
+#' @param crs Numérique. Identifiant EPSG du CRS de sortie. Par défaut le CRS de sortie
+#'     est le même que le shapefile d'origie.
 #'
 #' @importFrom utils download.file unzip
-#' @importFrom sf st_as_sf
+#' @importFrom sf st_as_sf st_transform
 #' @importFrom rgdal readOGR
 #'
 #' @return Un objet sf polygone des départements.
@@ -18,7 +20,7 @@
 #' url <- "https://www.data.gouv.fr/fr/datasets/r/3096e551-c68d-40ce-8972-a228c94c0ad1"
 #' depts <- osm_depts_tod(url = url, repertoire = "raw_data")
 #' }
-osm_depts_tod <- function(url, repertoire) {
+osm_depts_tod <- function(url, repertoire, crs = NA) {
 
   # Téchargement du shapefile des contours des départements
   fichier_destination <- paste0(repertoire, "/departements.zip")
@@ -39,6 +41,13 @@ osm_depts_tod <- function(url, repertoire) {
 
   depts <- rgdal::readOGR(shp_depts, use_iconv = TRUE) %>%
     sf::st_as_sf()
+
+  if(!is.na(crs))
+
+  {
+    depts <- depts %>%
+      sf::st_transform(crs = crs)
+  }
 
   depts
 
