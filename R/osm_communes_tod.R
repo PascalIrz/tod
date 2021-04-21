7d15983330aff05c0b712819a7267515588b053c
PascalIrz <pascal.irz@gmail.com>
2021-04-21 11:18:05 +0200

    ajout osm_communes_tod()

---
+++ R/osm_communes_tod.R
@@ -0,0 +1,51 @@
+#' Télécharger les contours des communes
+#'
+#' Cette fonction télécharge depuis la plateforme data.gouv.fr une couche simplifiée
+#'     des contours des communes au 01/01/19 et la décompresse. La source est OpenStreetMap.
+#'
+#' @param url Texte. URL de téléchargement du fichier GeoJSON.
+#' @param repertoire Texte. Chemin vers le répertoire où le fichier sera stocké.
+#' @param crs_sortie Numérique. Identifiant EPSG du CRS de sortie. Par défaut le CRS de sortie
+#'     est le même que celui du shapefile d'origine.
+#'
+#' @importFrom utils download.file unzip
+#' @importFrom sf read_sf st_transform
+#'
+#' @return Un objet sf polygone des contours des communes
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' url <- "https://www.data.gouv.fr/fr/datasets/r/07b7c9a2-d1e2-4da6-9f20-01a7b72d4b12"
+#' contours <- osm_communes_tod(url = url, repertoire = "raw_data", crs_sortie = 2154)
+#' }
+osm_communes_tod <- function(url, repertoire, crs_sortie = NA)
+
+{
+  # Téchargement du shapefile des contours des départements
+  fichier_destination <- paste0(repertoire, "/communes-20190101.zip")
+
+  download.file(url = url,
+                destfile = fichier_destination,
+                mode = "wb")
+
+  # Décompression
+  unzip(zipfile = fichier_destination,
+        exdir = repertoire)
+
+  geojson_communes <- paste0(repertoire, "/communes-20190101.json")
+
+  communes <- sf::read_sf(geojson_communes)
+
+  if (!is.na(crs_sortie))
+
+  {
+
+    communes <- communes %>%
+      sf::st_transform(crs = crs_sortie)
+
+  }
+
+  communes
+
+}
